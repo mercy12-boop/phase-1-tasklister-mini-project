42094249b64050012376cd96ff77d042429b6d1f
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-04-17 16:38:27 +0300

    Completes Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", addTask);
 });
+
+function addTask(e) {
+  e.preventDefault();
+  const taskList = document.getElementById("tasks"); // grabbing the container containing the tasks
+  const tasks = document.createElement("li"); // creating a way to display the tasks
+  tasks.textContent = e.target.description.value;
+  taskList.append(tasks);
+  e.target.reset(); // clearing out the input
+  // e.target.description.value = ""; Achieves the sale thing
+  const button = document.createElement("button");
+  button.textContent = "X";
+  button.style.marginLeft = "10px";
+  tasks.appendChild(button);
+  button.addEventListener("click", deleteTasks);
+}
+
+function deleteTasks(e) {
+  const task = e.target.parentNode;
+  task.remove();
+}
